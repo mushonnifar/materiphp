1c9144c29c917e0055119a2b93bfd23897d624c3
Mushonnif Ahmad <mushonnif3@gmail.com>
2019-09-18 15:44:57 +0700

    add Mobil class

---
+++ OOP/Mobil.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Description of Class
+ *
+ * @author Lenovo
+ */
+class Mobil {
+
+    //Property
+    private $jumlahRoda;
+    //Property
+    private $jumlahKursi;
+
+    //Method
+    public function setJumlahRoda($jumlahRoda) {
+        $this->jumlahRoda = $jumlahRoda;
+    }
+
+    //Method
+    public function setJumlahKursi($jumlahKursi) {
+        $this->jumlahKursi = $jumlahKursi;
+    }
+
+    //Method
+    public function cetak() {
+        echo 'Mobil punya ' . $this->jumlahRoda . ' roda dan ' . $this->jumlahKursi . ' kursi.';
+    }
+
+}
+
+$sedan = new Mobil(); //Object
+$sedan->setJumlahRoda(4);
+$sedan->setJumlahKursi(6);
+$sedan->cetak();
