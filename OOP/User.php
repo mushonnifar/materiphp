5f2879c51939c7f28d6bbbc0e1849f288438ab24
Mushonnif Ahmad <mushonnif3@gmail.com>
2019-09-20 20:47:00 +0700

    Constant

---
+++ OOP/User.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of User
+ *
+ * @author Lenovo
+ */
+class User {
+
+    const JK = "Perempuan";
+
+    public function getJK() {
+        return self::JK;
+    }
+
+}
+
+$user = new User();
+
+//memanggil konstanta dari method
+echo $user->getJK() . '<br>';
+//memanggil konstanta dengan double colon
+echo $user::JK;
